e9a24816a8d632e45f44c5358cdf75e8229cea11
DESKTOP-BOD8RE1\dwest <dwest.netdev@gmail.com>
2017-11-12 05:38:41 -0600

    fix : add all constructors for stored procedure commands/queries to match base class constructors; clean up csproj x 2

--- FluidDbClient/src/FluidDbClient/Controls/ManagedDbCommand/StoredProcedureDbCommand.cs
+++ FluidDbClient/src/FluidDbClient/Controls/ManagedDbCommand/StoredProcedureDbCommand.cs
@@ -1,13 +1,30 @@
 ﻿using System.Data;
+using System.Data.Common;
 
 namespace FluidDbClient
 {
-    // TODO: more constructor options
-
     public abstract class StoredProcedureDbCommandBase : ManagedDbCommand
     {
+        protected StoredProcedureDbCommandBase(Database database, string storedProcedureName, object parameters) 
+            : base(database, parameters)
+        {
+            StoredProcedureName = storedProcedureName;
+        }
+
         protected StoredProcedureDbCommandBase(Database database, DbSessionBase session, string storedProcedureName, object parameters)
             : base(database, session, parameters)
+        {
+            StoredProcedureName = storedProcedureName;
+        }
+        
+        protected StoredProcedureDbCommandBase(Database database, DbConnection connection, string storedProcedureName, object parameters)
+            : base(database, connection, parameters)
+        {
+            StoredProcedureName = storedProcedureName;
+        }
+
+        protected StoredProcedureDbCommandBase(Database database, DbTransaction transaction, string storedProcedureName, object parameters)
+            : base(database, transaction, parameters)
         {
             StoredProcedureName = storedProcedureName;
         }
@@ -26,24 +43,40 @@ namespace FluidDbClient
 
     public class StoredProcedureDbCommand : StoredProcedureDbCommandBase
     {
+        public StoredProcedureDbCommand(string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase(), storedProcedureName, parameters)
+        { }
+
         public StoredProcedureDbCommand(DbSessionBase session, string storedProcedureName, object parameters = null) 
             : base(DbRegistry.GetDatabase(), session, storedProcedureName, parameters)
         { }
 
-        public StoredProcedureDbCommand(string storedProcedureName, object parameters = null)
-            : this(null, storedProcedureName, parameters)
+        public StoredProcedureDbCommand(DbConnection connection, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase(), connection, storedProcedureName, parameters)
+        { }
+
+        public StoredProcedureDbCommand(DbTransaction transaction, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase(), transaction, storedProcedureName, parameters)
         { }
     }
 
 
     public class StoredProcedureDbCommand<TDatabase> : StoredProcedureDbCommandBase where TDatabase : Database
     {
-        public StoredProcedureDbCommand(DbSessionBase session, string storedProcedureName, object parameters = null)
-                    : base(DbRegistry.GetDatabase<TDatabase>(), session, storedProcedureName, parameters)
+        public StoredProcedureDbCommand(string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase<TDatabase>(), storedProcedureName, parameters)
         { }
 
-        public StoredProcedureDbCommand(string storedProcedureName, object parameters = null)
-            : this(null, storedProcedureName, parameters)
+        public StoredProcedureDbCommand(DbSessionBase session, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase<TDatabase>(), session, storedProcedureName, parameters)
+        { }
+
+        public StoredProcedureDbCommand(DbConnection connection, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase<TDatabase>(), connection, storedProcedureName, parameters)
+        { }
+        
+        public StoredProcedureDbCommand(DbTransaction transaction, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase<TDatabase>(), transaction, storedProcedureName, parameters)
         { }
     }
 }
--- FluidDbClient/src/FluidDbClient/Controls/ManagedDbQuery/StoredProcedureDbQuery.cs
+++ FluidDbClient/src/FluidDbClient/Controls/ManagedDbQuery/StoredProcedureDbQuery.cs
@@ -1,13 +1,30 @@
 ﻿using System.Data;
+using System.Data.Common;
 
 namespace FluidDbClient
 {
-    // TODO: more constructor options
-
     public abstract class StoredProcedureDbQueryBase : ManagedDbQuery
     {
+        protected StoredProcedureDbQueryBase(Database database, string storedProcedureName, object parameters) 
+            : base(database, parameters)
+        {
+            StoredProcedureName = storedProcedureName;
+        }
+
         protected StoredProcedureDbQueryBase(Database database, DbSessionBase session, string storedProcedureName, object parameters) 
             : base(database, session, parameters)
+        {
+            StoredProcedureName = storedProcedureName;
+        }
+
+        protected StoredProcedureDbQueryBase(Database database, DbConnection connection, string storedProcedureName, object parameters)
+            : base(database, connection, parameters)
+        {
+            StoredProcedureName = storedProcedureName;
+        }
+
+        protected StoredProcedureDbQueryBase(Database database, DbTransaction transaction, string storedProcedureName, object parameters)
+            : base(database, transaction, parameters)
         {
             StoredProcedureName = storedProcedureName;
         }
@@ -26,23 +43,39 @@ namespace FluidDbClient
 
     public class StoredProcedureDbQuery : StoredProcedureDbQueryBase
     {
+        public StoredProcedureDbQuery(string storedProcedureName, object parameters = null) 
+            : base(DbRegistry.GetDatabase(), storedProcedureName, parameters)
+        { }
+
         public StoredProcedureDbQuery(DbSessionBase session, string storedProcedureName, object parameters = null) 
             : base(DbRegistry.GetDatabase(), session, storedProcedureName, parameters)
         { }
 
-        public StoredProcedureDbQuery(string storedProcedureName, object parameters = null) 
-            : this(null, storedProcedureName, parameters)
+        public StoredProcedureDbQuery(DbConnection connection, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase(), connection, storedProcedureName, parameters)
+        { }
+
+        public StoredProcedureDbQuery(DbTransaction transaction, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase(), transaction, storedProcedureName, parameters)
         { }
     }
 
     public class StoredProcedureDbQuery<TDatabase> : StoredProcedureDbQueryBase where TDatabase : Database
     {
+        public StoredProcedureDbQuery(string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase<TDatabase>(), storedProcedureName, parameters)
+        { }
+
         public StoredProcedureDbQuery(DbSessionBase session, string storedProcedureName, object parameters = null)
             : base(DbRegistry.GetDatabase<TDatabase>(), session, storedProcedureName, parameters)
         { }
 
-        public StoredProcedureDbQuery(string storedProcedureName, object parameters = null)
-            : this(null, storedProcedureName, parameters)
+        public StoredProcedureDbQuery(DbConnection connection, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase<TDatabase>(), connection, storedProcedureName, parameters)
+        { }
+
+        public StoredProcedureDbQuery(DbTransaction transaction, string storedProcedureName, object parameters = null)
+            : base(DbRegistry.GetDatabase<TDatabase>(), transaction, storedProcedureName, parameters)
         { }
     }
 }
