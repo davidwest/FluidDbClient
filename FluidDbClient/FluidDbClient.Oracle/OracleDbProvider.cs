ecb7e18122500bbf29f70a6975f654d9b4e7e0b4
davidwest <dwest.netdev@gmail.com>
2019-06-18 05:11:54 -0500

    add FluidDbClient.Oracle

---
+++ FluidDbClient/FluidDbClient.Oracle/OracleDbProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+using Oracle.ManagedDataAccess.Client;
+
+namespace FluidDbClient.Oracle
+{
+    internal class OracleDbProvider : IDbProvider
+    {
+        private readonly string _connectionString;
+
+        public OracleDbProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbConnection CreateConnection() => new OracleConnection(_connectionString);
+
+        public DbParameter CreateParameter(string name, object value) => OracleParameterFactory.CreateParameter(name, value);
+
+        public DbCommand CreateCommand(DbConnection connection)
+        {
+            var cmd = connection.CreateCommand();
+
+            ((OracleCommand) cmd).BindByName = true;
+
+            return cmd;
+        }
+
+        public string ProviderId => "Oracle.ManagedDataAccess.Client";
+
+        public IDbProviderValueInterpreter Interpreter => new OracleValueInterpreter();
+    }
+}
---
+++ FluidDbClient/FluidDbClient.Oracle/OracleValueInterpreter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using Oracle.ManagedDataAccess.Client;
+
+namespace FluidDbClient.Oracle
+{
+    public class OracleValueInterpreter : DbProviderValueInterpreter
+    {
+        public override string GetDiagnosticString(DbParameter parameter)
+        {
+            var sqlParam = (OracleParameter)parameter;
+
+            // TODO
+            return sqlParam.ToString();
+        }
+
+        public override bool CanEvaluateAsMultiParameters(object value)
+        {
+            return base.CanEvaluateAsMultiParameters(value) &&
+                   !(value is IEnumerable<OracleParameter>);
+        }
+    }
+}
